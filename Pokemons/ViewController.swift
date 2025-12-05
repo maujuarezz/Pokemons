2af038f7da062cd80192a260bcc26a5f8dfa3a94
Mauro Juárez Zavaleta <maujuarez86@gmail.com>
2025-12-04 18:58:36 -0600

    se agrega funcionalidad de carga al inicio y fin de scroll

--- Pokemons/ViewController.swift
+++ Pokemons/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, ViewGeneral {
+class ViewController: UIViewController, ViewGeneral, UIScrollViewDelegate {
         
     /*var dataSource: [Model] = [
         Model(id: 1, title: "Title 1", subtitle: "Subtitle 1"),
@@ -15,9 +15,16 @@ class ViewController: UIViewController, ViewGeneral {
         Model(id: 3, title: "Title 3", subtitle: "Subtitle 3")
     ]*/
     var dataSource: [Pokemon] = []
+    var previousUrl: String = ""
+    var nextUrl: String = ""
+    
+    @IBOutlet weak var containerView: UIScrollView!
+
     
     lazy var tableView: UITableView = {
         let tableView = UITableView()
+
+        
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: CustomTableViewCell.identifier)
@@ -28,10 +35,13 @@ class ViewController: UIViewController, ViewGeneral {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+
         setupView()
         addSubviews()
         setupConstraints()
-        fetchPokemons()
+        fetchPokemons(urlPokemons: "https://pokeapi.co/api/v2/pokemon")
+        
     }
     func setupView() {
         view.backgroundColor = .cyan
@@ -47,8 +57,8 @@ class ViewController: UIViewController, ViewGeneral {
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
-    func fetchPokemons() {
-        let urlString = "https://pokeapi.co/api/v2/pokemon"
+    func fetchPokemons(urlPokemons: String) {
+        let urlString = urlPokemons
         guard let url = URL(string: urlString) else {
             return
         }
@@ -67,7 +77,16 @@ class ViewController: UIViewController, ViewGeneral {
                 let pokemons: PokemonResponse = try
                     decoder.decode(PokemonResponse.self, from: data)
                 DispatchQueue.main.async {
-                    self.successPokemon(response: pokemons)
+                    self.dataSource = pokemons.results
+                    self.tableView.reloadData()
+                    
+                    
+                    self.nextUrl = pokemons.next
+                    guard let previa = pokemons.previous else {
+                        return
+                    }
+                    self.previousUrl = previa
+                    
                 }
                 
             } catch {
@@ -75,10 +94,27 @@ class ViewController: UIViewController, ViewGeneral {
             }
         }.resume()
     }
-    func successPokemon(response: PokemonResponse) {
-        dataSource = response.results
-        tableView.reloadData()
+
+    // función para detectar el final de scroll en pantalla
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        print("Scrolling ended after deceleration.")
+        // Example: Load more data if scrolled near the end
+        if scrollView.contentOffset.y >= (scrollView.contentSize.height - scrollView.frame.size.height) {
+            print("Reached the bottom of the scroll view.")
+            // Trigger load more data
+            fetchPokemons(urlPokemons: self.nextUrl)
+        } else {
+            print("Inicio")
+            fetchPokemons(urlPokemons: self.previousUrl)
+        }
     }
+
+   /** func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if !decelerate {
+            print("Scrolling ended immediately after dragging.")
+            // Perform actions if scrolling stopped without deceleration
+        }
+    }*/
     
 }
 
